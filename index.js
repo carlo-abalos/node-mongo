0169c73a139a95c62e603cd76481499da0e6fd29
Carlo Abalos <carloabalos24@gmail.com>
2018-03-01 15:00:54 +0800

    Node and MongoDB Example Part 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-
+const dboper = require('./operations');
 const url = 'mongodb://localhost:27017';
 
 MongoClient.connect(url, (err, client) => {
@@ -12,7 +12,33 @@ MongoClient.connect(url, (err, client) => {
 
 	const db = client.db('conFusion');
 
-	const collection = db.collection('dishes');
+	dboper.insertDocument(db, { name: "Carlo", description: "Test"},
+		"dishes", (result) => {
+			console.log("Insert Document:\n", result.ops);
+
+			dboper.findDocuments(db, "dishes", (docs) => {
+				console.log("Found Documents:\n", docs);
+
+				dboper.updateDocument(db, {name: "Carlo"},
+					{description: "Updated Test"}, "dishes",
+					(result) => {
+						console.log("Updated Document:\n", result.result);
+
+						
+					dboper.findDocuments(db, "dishes", (docs) => {
+						console.log("Found Updated Documents:\n", docs);
+
+						db.dropCollection("dishes", (result) => {
+							console.log("Dropped Collection: ", result);
+
+							client.close();
+						});
+					});
+				});
+			});
+		});	
+
+	/*const collection = db.collection('dishes');
 	collection.insertOne({"name": "Carlo", "description": "test"},
 		(err, result) => {
 			assert.equal(err,null);
@@ -32,6 +58,6 @@ MongoClient.connect(url, (err, client) => {
 					client.close();
 				});
 			});
-		});
+		});*/
 
 });
